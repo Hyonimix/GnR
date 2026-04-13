f4cd9cc2e69a7fa2677caa8ecc7df8c434610c11
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2026-04-13 10:03:40 +0900

    1.2.0

--- Run.bat
+++ Run.bat
@@ -1,3 +1,8 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0GnR_bin.ps1"
-exit
+cd /d "%~dp0"
+
+set "vbs=%temp%\run_grep.vbs"
+echo Set objShell = WScript.CreateObject("WScript.Shell") > "%vbs%"
+echo objShell.Run "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -Command ""Invoke-Command -ScriptBlock ([scriptblock]::Create([System.IO.File]::ReadAllText('GnR_bin.ps1', [System.Text.Encoding]::UTF8)))""", 0, False >> "%vbs%"
+
+wscript.exe "%vbs%"
